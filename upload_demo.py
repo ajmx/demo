63b7a203398934b2bd7096e00991aafc57c7f1e6
Alan <alan@armona.co.uk>
2017-09-13 08:27:18 +1000

    File upload script added

---
+++ upload_demo.py
@@ -0,0 +1,16 @@
+import boto3
+import StringIO
+import zipfile
+
+s3 = boto3.resource('s3')
+demo_bucket = s3.Bucket('demo.ajumo.co.uk')
+build_bucket = s3.Bucket('demo.build.ajumo.co.uk')
+
+demo_zip = StringIO.StringIO()
+build_bucket.download_fileobj('demo_ajumo_build.zip',demo_zip)
+
+with zipfile.ZipFile(demo_zip) as zfile:
+    for fname in zfile.namelist():
+        obj = zfile.open(fname)
+        demo_bucket.upload_fileobj(obj,fname)
+        demo_bucket.Object(fname).Acl().put(ACL='public-read')
