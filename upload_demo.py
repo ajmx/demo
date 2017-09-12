1b821a0a1abe73ac6cbbf327ddc06fc4ce2abb5f
Alan <alan@armona.co.uk>
2017-09-13 08:28:06 +1000

    Support for mime type added

--- upload_demo.py
+++ upload_demo.py
@@ -1,6 +1,7 @@
 import boto3
 import StringIO
 import zipfile
+import mimetypes
 
 s3 = boto3.resource('s3')
 demo_bucket = s3.Bucket('demo.ajumo.co.uk')
@@ -12,5 +13,6 @@ build_bucket.download_fileobj('demo_ajumo_build.zip',demo_zip)
 with zipfile.ZipFile(demo_zip) as zfile:
     for fname in zfile.namelist():
         obj = zfile.open(fname)
-        demo_bucket.upload_fileobj(obj,fname)
+        demo_bucket.upload_fileobj(obj,fname, 
+            ExtraArgs={'ContentType':mimetypes.guess_type(fname)[0]})
         demo_bucket.Object(fname).Acl().put(ACL='public-read')
